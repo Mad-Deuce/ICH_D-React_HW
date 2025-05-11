dd66fed37d916d5a42a5e6556b234a2c81011753
zolotukhin_petro <zolotukhinpv@gmail.com>
2025-05-11 19:50:44 +0300

    hw_06

---
+++ hw_06/src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import UserProfile from "/src/components/UserProfile/UserProfile";
+
+import "/src/styles/styles.css";
+
+export default function App() {
+  return (
+    <>
+      <UserProfile />
+    </>
+  );
+}
