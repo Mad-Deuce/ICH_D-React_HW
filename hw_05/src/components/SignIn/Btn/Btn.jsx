c525b7eb622939475d7b640e9508f66b9303b2f3
zolotukhin_petro <zolotukhinpv@gmail.com>
2025-05-23 16:45:23 +0300

    hw_09

--- hw_05/src/components/SignIn/Btn/Btn.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Btn.module.css";
-
-export default function Btn({ href = "/", children }) {
-  return (
-    <a href={href} className={styles.link}>
-      {children}
-    </a>
-  );
-}
